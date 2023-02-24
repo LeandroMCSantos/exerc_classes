248b47befa3a78cd109c74054223e8be42d2fa9f
Leandro <102751955+LeandroMCSantos@users.noreply.github.com>
2023-02-24 10:02:22 -0300

    Add files via upload

---
+++ criando objeto/exerc1.js
@@ -0,0 +1,34 @@
+let bruno ={
+    nome: "Bruno",
+    idade: 32,
+    academia: true,
+}
+
+//exerc2
+
+let pessoa ={
+    nome:'augusto',
+    mostrar: () => {
+        console.log([pessoa.nome])
+    }
+
+}
+
+//exerc3
+
+let mulher ={
+    nome:'maria',
+    idade1:17,
+}
+
+let homem ={
+    nome:'joao',
+    idade2: 16,
+}
+
+if(idade1 > idade2){
+    nomemaisvelho = nome1;
+}else{
+    nomemaisvelho = nome2;
+}
+console.log(`a pessoa mais velha é ${nomemaisvelho}.`);
---
+++ criando objeto/script.js
@@ -0,0 +1,12 @@
+//let carro = {
+  //  portas: 4,
+  //  rodas: 4,
+  //  vidros: 6,
+  //  vendido: true,
+  //  valor: 239000,
+    //marca:'BMW'
+    //acessorios:[                                                                                                                                                                                                            
+      //  'ar condicionado','tv','freio a disco','nitro'
+    //]
+//}
+//console.log(carro.marca)//
